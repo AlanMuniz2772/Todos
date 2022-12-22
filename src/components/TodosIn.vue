53e333c5fc3e1e4612fb64c9bebdc400f96dfaba
Alan <jorgeamr2772@gmail.com>
2022-12-22 11:22:53 -0600

    first comiit

---
+++ src/components/TodosIn.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <div v-bind:key="todo.id" v-for="todo in todoslist">
+            <div>{{todo.title}}</div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'TodosIn',
+        props: ['todoslist']
+    }
+</script>
+
+<style></style>
